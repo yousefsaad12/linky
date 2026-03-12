018c87aee517667ab1b842d411174efe899646c7
Yousef Saad <saadrslan95@gmail.com>
2026-03-12 16:49:46 +0000

    getAllUrls endpoint added and its route

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -2,6 +2,22 @@ require("dotenv").config({ path: "./config.env" });
 const { nanoid } = require("nanoid");
 const Url = require("./../models/urlModel");
 
+exports.getAllUrls = async (req, res) => {
+  try {
+    const urls = await Url.find();
+
+    res.status(200).json({
+      status: "success",
+      data: urls,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
 exports.createShortUrl = async (req, res) => {
   try {
     const originalUrl = req.body;
@@ -22,6 +38,3 @@ exports.createShortUrl = async (req, res) => {
     });
   }
 };
-
-
-
--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -2,4 +2,7 @@ const express = require("express");
 const urlController = require("./../controllers/urlController");
 const router = express.Router();
 
-router.route("/").post(urlController.createShortUrl);
+router
+  .route("/")
+  .post(urlController.createShortUrl)
+  .get(urlController.getAllUrls);

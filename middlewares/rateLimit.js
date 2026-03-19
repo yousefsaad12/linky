c8a8ea92273c038aa575b6d0c3a3ed4d9d82a4c7
Yousef Saad <saadrslan95@gmail.com>
2026-03-19 19:53:26 +0000

    Rate Limit refact

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -1,25 +1,35 @@
 const rateLimit = require("express-rate-limit");
+const AppError = require("./../utils/AppError");
 
 exports.redirectLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 minute
-  max: 1000,
+  limit: 1000,
   standardHeaders: true,
   legacyHeaders: false,
-  message: "Too many requests, please slow down.",
+
+  handler: (req, res, next) => {
+    return next(new AppError("Too many requests, please slow down.", 429));
+  },
 });
 
 exports.createUrlLimiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 30, // protect system from spam
+  limit: 30, // protect system from spam
   standardHeaders: true,
   legacyHeaders: false,
-  message: "Too many URLs created from this IP, try later.",
+  handler: (req, res, next) => {
+    return next(
+      new AppError("Too many URLs created from this IP, try later.", 429),
+    );
+  },
 });
 
 exports.analyticsLimiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 100,
+  limit: 100,
   standardHeaders: true,
   legacyHeaders: false,
-  message: "Too many requests, please slow down.",
+  handler: (req, res, next) => {
+    return next(new AppError("Too many requests, please slow down.", 429));
+  },
 });

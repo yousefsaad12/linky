fb1e640d6a4e4326d263843b836277bd5e0a234d
Yousef Saad <saadrslan95@gmail.com>
2026-05-15 16:45:23 +0300

    fixing bugs

--- config/passport.js
+++ config/passport.js
@@ -2,14 +2,14 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const User = require("../models/userModel");
-const AppError = require("../utils/appError");
+
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/api/v1/auth/google/callback",
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {

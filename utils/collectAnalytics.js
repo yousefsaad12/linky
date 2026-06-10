d53773b8e4c0a580918065f414b7d2a05397df05
Yousef Saad <saadrslan95@gmail.com>
2026-06-10 18:06:21 +0300

    fixing bugs

--- utils/collectAnalytics.js
+++ utils/collectAnalytics.js
@@ -2,29 +2,27 @@ const { UAParser } = require("ua-parser-js");
 const geoip = require("geoip-lite");
 
 const collectAnalytics = (req) => {
-  // Parse User-Agent
   const parser = new UAParser(req.headers["user-agent"]);
   const device = parser.getDevice().type;
 
   const forwarded = req.headers["x-forwarded-for"];
-  const ip = forwarded
+  let ip = forwarded
     ? forwarded.split(",")[0].trim()
     : req.socket.remoteAddress;
 
-  // Geo lookup
+  if (ip?.startsWith("::ffff:")) {
+    ip = ip.substring(7);
+  }
+
   const geo = geoip.lookup(ip);
 
   return {
     os: parser.getOS().name || "unknown",
     browser: parser.getBrowser().name || "unknown",
     deviceType:
-      device === "mobile"
-        ? "mobile"
-        : device === "tablet"
-          ? "tablet"
-          : device
-            ? "desktop"
-            : "unknown",
+      device === "mobile" ? "mobile"
+      : device === "tablet" ? "tablet"
+      : "desktop",
     referrer: req.headers["referer"] || "direct",
     region: geo?.region || "unknown",
     city: geo?.city || "unknown",
